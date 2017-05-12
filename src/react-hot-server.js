4658ba45f4f3fa045614e4520a496c706c65c6f9
Vlad Zhukov <work4vlad@gmail.com>
2017-05-12 17:18:36 +0300

    Rework reactHotServer block

--- src/react-hot-server.js
+++ src/react-hot-server.js
@@ -1,25 +1,24 @@
 const {webpack} = require('@webpack-blocks/webpack2');
-const pathNode = require('path');
+const path = require('path');
 
 /*
  * Webpack dev server with React HMR.
  */
 module.exports = (options = {}) => {
-    const {host = 'localhost', port = 3000, path = ''} = options;
+    const {host = 'localhost', port = 8080, publicPath = '/', ...rest} = options;
+
+    /* eslint-disable no-underscore-dangle */
+    const _url = `http://${host}:${port}`;
+    const _path = path.resolve(__dirname, publicPath);
+    const _publicPath = `${_url}${publicPath}`;
+    /* eslint-enable no-underscore-dangle */
 
     const reactHotServer = () => ({
-        entry: [
-            'react-hot-loader/patch',
-            `webpack-dev-server/client?http://${host}:${port}`,
-            'webpack/hot/only-dev-server',
-        ],
         output: {
-            path: pathNode.resolve(__dirname, path),
-            publicPath: `http://${host}:${port}/${path}`,
+            path: _path,
+            publicPath: _publicPath,
         },
-    });
 
-    const postConfig = () => ({
         plugins: [
             new webpack.HotModuleReplacementPlugin(),
             new webpack.NamedModulesPlugin(),
@@ -29,10 +28,36 @@ module.exports = (options = {}) => {
         devServer: {
             host,
             port,
+            contentBase: _path,
+            publicPath: _publicPath,
             historyApiFallback: true,
             hot: true,
+            ...rest,
         },
     });
+
+    function postConfig(context, config) {
+        const devServerEntry = [
+            'react-hot-loader/patch',
+            `webpack-dev-server/client?${_url}`,
+            'webpack/hot/only-dev-server',
+        ];
+
+        const presentEntry = config.entry || '';
+
+        let entry = {};
+
+        if (typeof presentEntry === 'string' || Array.isArray(presentEntry)) {
+            entry = devServerEntry.concat(presentEntry);
+        }
+        else {
+            Object.keys(presentEntry).forEach((chunkName) => {
+                entry[chunkName] = devServerEntry.concat(presentEntry[chunkName]);
+            });
+        }
+
+        return {entry};
+    }
 
     return Object.assign(reactHotServer, {
         post: postConfig,
